698fcca280915063862758eb1d1c8d2d86fd0cf3
AakritiShresthaShrestha <Aakritibikash.stha88@gmail.com>
2021-02-10 19:55:48 -0500

    Completed the Products web application using springBoot perform CRUD operations and used POSTMAN too

---
+++ src/main/java/SpringCRUDweek/Project/ProductService.java
@@ -0,0 +1,15 @@
+package SpringCRUDweek.Project;
+
+import java.util.List;
+
+public interface ProductService {
+    Product createProduct(Product product);
+
+    Product updateProduct(Product product);
+
+    List < Product > getAllProduct();
+
+    Product getProductById(long productId);
+
+    void deleteProduct(long id);
+}
---
+++ src/main/java/SpringCRUDweek/Project/ResourceNotFoundException.java
@@ -0,0 +1,17 @@
+package SpringCRUDweek.Project;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1l;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}
